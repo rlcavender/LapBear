c7a9cddc8d2bf1314abeed4e89f9a48e30f592a8
Rachel Cavender <rlcavender36@gmail.com>
2023-02-25 09:02:41 -0600

    Add start/stop race buttons

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,23 @@ function App() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("/api")
+    fetch("/home")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
+
+  function handleClick(route) {
+    fetch(route)
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => { handleClick("/startRace") }}>Start Race</button>
+        <button onClick={() => { handleClick("/endRace") }}>End Race</button>
         <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
-// server/index.js
-
-const path = require('path');
+const path = require("path");
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -8,16 +6,24 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
+// Handle GET requests
+app.get("/home", (req, res) => {
   res.json({ message: "LapBear! vroom vroom ʕ •ᴥ•ʔ" });
+});
+
+app.get("/startRace", (req, res) => {
+  res.json({ message: "Listening for race data" });
+});
+
+app.get("/endRace", (req, res) => {
+  res.json({ message: "Stopped listening for race data" });
 });
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
